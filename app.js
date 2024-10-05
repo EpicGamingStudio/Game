89d1e022937af620dab26001baaa243e336140d5
EpicGamingStudio <kmunaf994@gmail.com>
2024-10-05 17:44:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -30,4 +30,31 @@ window.onload = function() {
         document.getElementById('profile-id').innerText = user.email;  // Using email as ID for simplicity
     }
 };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// Import additional Firebase services you plan to use
+import { getDatabase } from "firebase/database"; // For Realtime Database
+import { getAuth } from "firebase/auth"; // For Authentication
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8LPmmQktqITK6PhkVBCb51JGqEi6rHZA",
+  authDomain: "gamedata-efb77.firebaseapp.com",
+  databaseURL: "https://gamedata-efb77-default-rtdb.firebaseio.com",
+  projectId: "gamedata-efb77",
+  storageBucket: "gamedata-efb77.appspot.com",
+  messagingSenderId: "452569523584",
+  appId: "1:452569523584:web:b607d5af9030d3d0fc0d65",
+  measurementId: "G-KEL28FPJMN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app); // Initialize the Realtime Database
+const auth = getAuth(app); // Initialize Firebase Authentication
+
+// You can now use `database` and `auth` for your app's functionality
 
